41c3f4bcd47c3d1f60abf0f97aefdbc672b6c560
Lucien Max Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-07-29 16:03:37 +0200

    Create 16x16 grid

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 /*Create 16x16 divs*/
-const div = document.createElement("div");
+const grid = document.querySelector('.grid');
+
+// Use a nested for loop to create the grid cells
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    grid.appendChild(cell);
+  }
+}

f9f510c20ec3b44dcbd8a618a7d34877e5c64c5f
Lucien Max Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-07-30 13:41:16 +0200

    Redo grid creation using flexbox

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-/*Use the container div in the HTML to put the grid into. The size of the grid is determined by the CSS class "grid".*/
-
 const grid = document.querySelector(".grid");
 
-/* Create 16 x 16 grid cells using a nested for loop. */
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
-    const cell = document.createElement("div");
-    /*Add the CSS class "cell" to every cell so that they are styled according to the stylesheet.*/
-    cell.classList.add("cell");
-    /*Add unique IDs to  each cell. The IDs are in the format "row number-column number". So the first cell on the left has the id "1-1", the last one on the right has the id "16-16".*/
-    cell.setAttribute("id", `${i + 1}-${j + 1}`);
-    /*Add an event listener to every cell that takes "mouseenter" as type and changeColor as callback function. */
-
-    cell.addEventListener("mouseenter", function () {
-      changeColor(cell.id);
+    const newCell = document.createElement("div");
+    newCell.classList.add("cell");
+  newCell.setAttribute("id", `${i + 1}-${j + 1}`);
+    newCell.addEventListener("mouseenter", function () {
+      changeColor(newCell.id);
     });
-
-    /*Inject the finished cells into the DOM!*/
-    grid.appendChild(cell);
+    grid.appendChild(newCell);
   }
 }
 

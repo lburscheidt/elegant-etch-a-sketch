0da3cef39d4916a072be8c721b95d8cba3533fab
Lucien Max Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-07-31 14:03:04 +0200

    Added functionality to reset grid

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 /*1. Get user input and store in a variable
 2. Calculate cell width = grid with / user input
 3. Create grid with event listeners as per v1*/
+
+document.documentElement.style.setProperty('--gridWidth', '480px');
+
+let gridWidth = window.getComputedStyle(document.documentElement).getPropertyValue('--gridWidth').replace(/[^0-9]/g, "");
+
 makeGrid(16);
 
 const resetBtn = document.getElementById("reset-btn");
 resetBtn.addEventListener("click", function () {
-  const numberOfcells = prompt("Please enter a number");
-  makeGrid(numberOfCells);
-});
+  resetGrid();
+  let numberOfCells = prompt("Please enter a number between 1 and 100");
+ 
+    makeGrid(numberOfCells);
+  }
+);
+
+function resetGrid(){
+  var cells = document.getElementsByClassName('small');
+
+  while(cells[0]) {
+      cells[0].parentNode.removeChild(cells[0]);
+};}
 
 function makeGrid(number) {
-  const calc = 480 / number;
+  
+  const calc = gridWidth / number;
   const cellWidth = `${calc}px`;
   console.log(cellWidth);
   document.documentElement.style.setProperty("--cellWidth", cellWidth);
@@ -21,6 +37,7 @@ function makeGrid(number) {
     for (let j = 0; j < number; j++) {
       const newCell = document.createElement("div");
       newCell.classList.add("cell");
+      newCell.classList.add("small");
       newCell.setAttribute("id", `${i + 1}-${j + 1}`);
       newCell.addEventListener("mouseenter", function () {
         changeColor(newCell.id);

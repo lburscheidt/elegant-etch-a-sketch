3f739edcb3e297d0f042e1f2d2751fc5c47ff151
Lucien Max Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-07-31 13:29:17 +0200

    Added user input and ability to make grid accordingly

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const grid = document.querySelector(".grid");
+/*1. Get user input and store in a variable
+2. Calculate cell width = grid with / user input
+3. Create grid with event listeners as per v1*/
+makeGrid(16);
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const newCell = document.createElement("div");
-    newCell.classList.add("cell");
-  newCell.setAttribute("id", `${i + 1}-${j + 1}`);
-    newCell.addEventListener("mouseenter", function () {
-      changeColor(newCell.id);
-    });
-    grid.appendChild(newCell);
+const resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", function () {
+  const numberOfcells = prompt("Please enter a number");
+  makeGrid(numberOfCells);
+});
+
+function makeGrid(number) {
+  const calc = 480 / number;
+  const cellWidth = `${calc}px`;
+  console.log(cellWidth);
+  document.documentElement.style.setProperty("--cellWidth", cellWidth);
+
+  const grid = document.querySelector(".grid");
+
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j < number; j++) {
+      const newCell = document.createElement("div");
+      newCell.classList.add("cell");
+      newCell.setAttribute("id", `${i + 1}-${j + 1}`);
+      newCell.addEventListener("mouseenter", function () {
+        changeColor(newCell.id);
+      });
+      grid.appendChild(newCell);
+    }
   }
 }
 
---
+++ script_.js
@@ -0,0 +1,48 @@
+const grid = document.querySelector(".grid");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const newCell = document.createElement("div");
+    newCell.classList.add("cell");
+    newCell.setAttribute("id", `${i + 1}-${j + 1}`);
+    newCell.addEventListener("mouseenter", function () {
+      changeColor(newCell.id);
+    });
+    grid.appendChild(newCell);
+  }
+};
+/*
+/*make resetbutton usable in JS */
+let resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", function(){
+  let number = prompt("Please enter a number";);
+  newGrid(number);
+};)
+/*when the button is clicked, open a prompt window to ask for a number */
+
+/*pass that number into the newGrid function as the argument */
+
+/*wrap grid creation in a function that takes a number as argument*/
+
+
+function newGrid(num){
+  /*add a function that removes the old grid at the start */
+  grid.removeChild();
+for (let i = 0; i < num; i++) {
+  for (let j = 0; j < num; j++) {
+    const newCell = document.createElement("div");
+    newCell.classList.add("cell");
+    newCell.setAttribute("id", `${i + 1}-${j + 1}`);
+    newCell.addEventListener("mouseenter", function () {
+      changeColor(newCell.id);
+    });
+    grid.appendChild(newCell);
+  }
+}
+}*/
+
+function changeColor(cellId) {
+  let cell = document.getElementById(cellId);
+  cell.classList.remove("cell");
+  cell.classList.add("cell-changed");
+}

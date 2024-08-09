cdf9863ca9df8a5843284d874e6f71ad7ef24d07
L Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-08-09 18:31:01 +0200

    Add darkening effect

--- script.js
+++ script.js
@@ -14,20 +14,25 @@ const standardBtn = document.getElementById("standard-btn");
 const darkBtn = document.getElementById("dark-btn");
 
 function changeDisco() {
+  clearGrid();
   currentMode = "disco";
 }
 
 function changeStandard() {
+  clearGrid();
   currentMode = "standard";
 }
 
-/*function changeDark() {
+function changeDark() {
+  clearGrid();
   currentMode = "dark";
-}*/
+}
 
 discoBtn.addEventListener("click", changeDisco);
 
 standardBtn.addEventListener("click", changeStandard);
+
+darkBtn.addEventListener("click", changeDark);
 
 function makeGrid(number) {
   document.documentElement.style.setProperty(
@@ -50,18 +55,24 @@ function makeGrid(number) {
 }
 
 makeGrid(16);
-const cells = document.querySelectorAll(".cell");
 
-clearBtn.addEventListener("click", () => {
+clearBtn.addEventListener("click", clearGrid);
+
+function clearGrid() {
+  const cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => {
     cell.style.backgroundColor = "white";
   });
-});
+}
 
 resetBtn.addEventListener("click", function () {
   resetGrid();
   let numberOfCells = prompt("Please enter a number between 1 and 100");
-  makeGrid(numberOfCells);
+  if (numberOfCells == null) {
+    makeGrid(16);
+  } else {
+    makeGrid(numberOfCells);
+  }
 });
 
 function resetGrid() {
@@ -79,18 +90,27 @@ function getRandomRgb() {
   return `rgba(${r}, ${g},${b})`;
 }
 
-/*function changeColor(cellId) {
-  let cell = document.getElementById(cellId);
-  let changedColor = getRandomRgb();
-  cell.style.backgroundColor = changedColor;
-}*/
-
 function changeColor(cellId) {
   let changedColor;
+  let cell = document.getElementById(cellId);
   if (currentMode == "standard") {
     changedColor = "#000";
-  } else {
+    cell.style.backgroundColor = changedColor;
+  } else if (currentMode == "disco") {
     changedColor = getRandomRgb();
+    cell.style.backgroundColor = changedColor;
+  } else {
+    changeOpacity(cellId);
   }
-  document.getElementById(cellId).style.backgroundColor = changedColor;
+}
+
+function changeOpacity(cellId) {
+  let changedOpacity;
+  let cell = document.getElementById(cellId);
+  cell.style.backgroundColor = "#000";
+  let currentOpacity = cell.style.opacity;
+  changedOpacity = currentOpacity -= '-0.1';
+  console.log(changedOpacity);
+cell.style.opacity = changedOpacity;
+  
 }

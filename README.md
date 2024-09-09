f69ae47f4f20034a3d4e513530615130c7a1f17b
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-09-09 20:15:19 +0200

    Add CSS reset

--- README.md
+++ README.md
@@ -1,15 +1,40 @@
 # Process
-This is a cross between a sketchpad and an etch-a-sketch. The spec/brief can be found at [the project's page on The Odin Project](https://www.theodinproject.com/lessons/foundations-etch-a-sketch)
 
-As per spec, I created a website that has a 16x16 grid of square divs on it when you first open it. The divs are created using a javascript loop and CSS flexbox. I then set up a javascript function that makes the divs change color on mouseenter. I first added a new class to the div, but found that this was very limited as it only worked for the "standard" change from white to black, not for the "extra credit" assignment of changing to random colors. So I rewrote the code when I got to that part of the assignment so the divs could change to random colors on mouseenter.
+This is a cross between a sketchpad and an etch-a-sketch. The spec/brief can be
+found at
+[the project's page on The Odin Project](https://www.theodinproject.com/lessons/foundations-etch-a-sketch)
 
-I added the popup that prompts users to enter a number between 1 and 100 for a new grid and also wrote a failsafe into the function that defaults to the standard 16x16 grid when "Cancel" is pressed or input is otherwise invalid.
+As per spec, I created a website that has a 16x16 grid of square divs on it when
+you first open it. The divs are created using a javascript loop and CSS flexbox.
+I then set up a javascript function that makes the divs change color on
+mouseenter. I first added a new class to the div, but found that this was very
+limited as it only worked for the "standard" change from white to black, not for
+the "extra credit" assignment of changing to random colors. So I rewrote the
+code when I got to that part of the assignment so the divs could change to
+random colors on mouseenter.
 
-I then moved on to the extra credit assignments. I changed the color change mechanism so the cells would change to a randomized color, but I wanted users to be able to toggle between the standard and the colourful mode, so I added buttons and corresponding functions. This came in useful when I moved on to the incremental change of opacity. I spent a very long time on this one because using the opacity property caused a lot of problems with resetting the grid, and also I wasn't happy with how it affected the outlines of the cells. So I came up with a solution that uses the alpha channel of the RGBA color model and I think it looks pretty neat.
+I added the popup that prompts users to enter a number between 1 and 100 for a
+new grid and also wrote a failsafe into the function that defaults to the
+standard 16x16 grid when "Cancel" is pressed or input is otherwise invalid.
+
+I then moved on to the extra credit assignments. I changed the color change
+mechanism so the cells would change to a randomized color, but I wanted users to
+be able to toggle between the standard and the colourful mode, so I added
+buttons and corresponding functions. This came in useful when I moved on to the
+incremental change of opacity. I spent a very long time on this one because
+using the opacity property caused a lot of problems with resetting the grid, and
+also I wasn't happy with how it affected the outlines of the cells. So I came up
+with a solution that uses the alpha channel of the RGBA color model and I think
+it looks pretty neat.
 
 # To Do
 
-I focused on the javascript functionality over making the site look great, but I'd like to style and structure it a bit more. I'd like to separate the "Clear" and "Reset" buttons from the ones that let you select a mode. I'd also like to add more modes such as an eraser and a color picker, and a mode that combines a user-chosen color and the darkening mode.  
+I focused on the javascript functionality over making the site look great, but
+I'd like to style and structure it a bit more. I'd like to separate the "Clear"
+and "Reset" buttons from the ones that let you select a mode. I'd also like to
+add more modes such as an eraser and a color picker, and a mode that combines a
+user-chosen color and the darkening mode.
 
-#Credit
-The Etch-a-Sketch favicon was created by Creaticca Creative Agency. Their work is available at  https://www.flaticon.com/authors/creaticca-creative-agency
+#Credit The Etch-a-Sketch favicon was created by Creaticca Creative Agency.
+Their work is available at
+https://www.flaticon.com/authors/creaticca-creative-agency
--- script.js
+++ script.js
@@ -13,97 +13,97 @@ standardBtn.addEventListener("click", changeStandard);
 document.documentElement.style.setProperty("--gridWidth", "400px");
 
 let gridWidth = window
-  .getComputedStyle(document.documentElement)
-  .getPropertyValue("--gridWidth")
-  .replace(/[^0-9]/g, "");
+	.getComputedStyle(document.documentElement)
+	.getPropertyValue("--gridWidth")
+	.replace(/[^0-9]/g, "");
 
 let currentMode = "standard";
 
 function changeDark() {
-  clearGrid();
-  currentMode = "dark";
+	clearGrid();
+	currentMode = "dark";
 }
 
 function changeDisco() {
-  clearGrid();
-  currentMode = "disco";
+	clearGrid();
+	currentMode = "disco";
 }
 
 function changeStandard() {
-  clearGrid();
-  currentMode = "standard";
+	clearGrid();
+	currentMode = "standard";
 }
 
 function makeGrid(number) {
-  document.documentElement.style.setProperty(
-    "--cellWidth",
-    `${gridWidth / number}px`,
-  );
+	document.documentElement.style.setProperty(
+		"--cellWidth",
+		`${gridWidth / number}px`
+	);
 
-  const grid = document.querySelector(".grid");
-  for (let i = 0; i < number; i++) {
-    for (let j = 0; j < number; j++) {
-      const newCell = document.createElement("div");
-      newCell.classList.add("cell");
+	const grid = document.querySelector(".grid");
+	for (let i = 0; i < number; i++) {
+		for (let j = 0; j < number; j++) {
+			const newCell = document.createElement("div");
+			newCell.classList.add("cell");
 
-      newCell.setAttribute("id", `${i + 1}XXX${j + 1}`);
-      newCell.addEventListener("mouseenter", function () {
-        changeColor(newCell.id);
-      });
-      grid.appendChild(newCell);
-    }
-  }
+			newCell.setAttribute("id", `${i + 1}XXX${j + 1}`);
+			newCell.addEventListener("mouseenter", function () {
+				changeColor(newCell.id);
+			});
+			grid.appendChild(newCell);
+		}
+	}
 }
 
 makeGrid(16);
 
 function clearGrid() {
-  const cells = document.querySelectorAll(".cell");
-  cells.forEach((cell) => {
-    cell.style.backgroundColor = "white";
-    cell.style.border = "1px solid black";
-    cell.style.opacity = "1.0";
-  });
+	const cells = document.querySelectorAll(".cell");
+	cells.forEach(cell => {
+		cell.style.backgroundColor = "white";
+		cell.style.border = "1px solid black";
+		cell.style.opacity = "1.0";
+	});
 }
 
 function resetGrid() {
-  var cells = document.getElementsByClassName("cell");
-  while (cells[0]) {
-    cells[0].parentNode.removeChild(cells[0]);
-  }
-  let numberOfCells = prompt("Please enter a number between 1 and 100");
-  if (numberOfCells == null) {
-    makeGrid(16);
-  } else {
-    makeGrid(numberOfCells);
-  }
+	var cells = document.getElementsByClassName("cell");
+	while (cells[0]) {
+		cells[0].parentNode.removeChild(cells[0]);
+	}
+	let numberOfCells = prompt("Please enter a number between 1 and 100");
+	if (numberOfCells == null) {
+		makeGrid(16);
+	} else {
+		makeGrid(numberOfCells);
+	}
 }
 
 function getRandomRgb() {
-  var num = Math.round(0xffffff * Math.random());
-  var r = num >> 16;
-  var g = (num >> 8) & 255;
-  var b = num & 255;
-  return `rgba(${r}, ${g},${b})`;
+	var num = Math.round(0xffffff * Math.random());
+	var r = num >> 16;
+	var g = (num >> 8) & 255;
+	var b = num & 255;
+	return `rgba(${r}, ${g},${b})`;
 }
 
 function changeColor(cellId) {
-  let changedColor;
-  let cell = document.getElementById(cellId);
-  if (currentMode == "standard") {
-    changedColor = "#000";
-    cell.style.backgroundColor = changedColor;
-  } else if (currentMode == "disco") {
-    changedColor = getRandomRgb();
-    cell.style.backgroundColor = changedColor;
-  } else {
-    let bgCol = cell.style.backgroundColor;
-    let currentAlpha = parseFloat(bgCol.split(",")[3]);
-    let updatedAlpha = Number(currentAlpha) + 0.1;
-    if (currentAlpha < 1.0) {
-      cell.style.backgroundColor = `rgba(0,0,0,${updatedAlpha})`;
-    } else {
-      cell.style.backgroundColor = `rgba(0,0,0,0.1)`;
-    }
-  }
+	let changedColor;
+	let cell = document.getElementById(cellId);
+	if (currentMode == "standard") {
+		changedColor = "#000";
+		cell.style.backgroundColor = changedColor;
+	} else if (currentMode == "disco") {
+		changedColor = getRandomRgb();
+		cell.style.backgroundColor = changedColor;
+	} else {
+		let bgCol = cell.style.backgroundColor;
+		let currentAlpha = parseFloat(bgCol.split(",")[3]);
+		let updatedAlpha = Number(currentAlpha) + 0.1;
+		if (currentAlpha < 1.0) {
+			cell.style.backgroundColor = `rgba(0,0,0,${updatedAlpha})`;
+		} else {
+			cell.style.backgroundColor = `rgba(0,0,0,0.1)`;
+		}
+	}
 }
